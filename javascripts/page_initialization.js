0f41fc300bc65dd8e7a814206df1d94adc0950dd
Elliot Reed <elliotreed2@gmail.com>
2015-08-07 17:39:06 -0700

    successfully added React object to page

--- javascripts/page_initialization.js
+++ javascripts/page_initialization.js
@@ -1,4 +1,25 @@
 ;(function () {
-'use strict';
+
+if (window.ECR === undefined) {
+  window.ECR = {};
+}
+
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+ECR.createCommentBox = function() {
+  React.render(
+    <CommentBox />,
+    document.getElementById('main-content-area')
+  );
+}
 
 })();
